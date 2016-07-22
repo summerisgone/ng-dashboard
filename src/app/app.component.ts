2568b53439c10d184581704cdce01133f930d1fb
Ivan Gromov <summer.is.gone@gmail.com>
2016-07-23 02:16:47 +0500

    Initialized as admin-lte components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HeaderComponent} from './header';
+import {SidebarComponent} from './sidebar';
+import {ContentComponent} from './content';
+import {FooterComponent} from './footer';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeaderComponent, SidebarComponent, ContentComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'app works!';
